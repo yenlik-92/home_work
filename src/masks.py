98f68c74db0f6a1f17d07075885c4472dff91af6
Enlik Almanbetova <enlik-92@mail.ru>
2025-05-19 22:25:52 +0300

    mask_account_card

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,19 @@
 def get_mask_card_number(card_number: str) -> str:
     """Эта функция возвращает замаскированный номер карты"""
-    mask = card_number[0:4] + " " + card_number[4:6] + "**" + " " + "****" + " " + card_number[12:]
+    mask = card_number[:-17] + " " + card_number[-17:-12] + " " + card_number[-12:-10] + "**" + " " + "****"  + " " + card_number[-4:]
     return mask
 
 
-card_number = str(input())
-print(get_mask_card_number(card_number))
+
 
 
 def get_mask_account(account_number: str) -> str:
     """Эта функция возвращает замаскированный номер счета"""
-    mask_account = "**" + account_number[-4:]
+    mask_account = "Счет" + " " + "**" + account_number[-4:]
     return mask_account
 
-
-account_number = str(input())
-print(get_mask_account(account_number))
+if __name__ == '__main__':
+    card_number = str("Visa Classic 6831982476737658")
+    print(get_mask_card_number(card_number))
+    account_number = "Счет 73654108430135874305"
+    print(get_mask_account(account_number))
---
+++ src/widget.py
@@ -0,0 +1,33 @@
+from masks import  get_mask_card_number, get_mask_account
+def mask_account_card(type_account_card):
+    """Эта функция для счета и карты и названия """
+    if "Счет" in type_account_card:
+        return  get_mask_account(type_account_card)
+    else:
+        return get_mask_card_number(type_account_card)
+
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+
+def get_date(data_str):
+    modified_data = data_str[8:10] + "." + data_str[5:7] + "." + data_str[0:4]
+    return modified_data
+
+
+
+print(get_date("2024-03-11T02:26:18.671407"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
