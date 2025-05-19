fbaa46810ca31df15a975819837e81ae6eb2fbaa
Enlik Almanbetova <enlik-92@mail.ru>
2025-05-19 22:41:49 +0300

    lint checked

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,18 @@
 def get_mask_card_number(card_number: str) -> str:
     """Эта функция возвращает замаскированный номер карты"""
-    mask = card_number[:-17] + " " + card_number[-17:-12] + " " + card_number[-12:-10] + "**" + " " + "****"  + " " + card_number[-4:]
+    mask = (
+        card_number[:-17]
+        + " "
+        + card_number[-17:-12]
+        + " "
+        + card_number[-12:-10]
+        + "**"
+        + " "
+        + "****"
+        + " "
+        + card_number[-4:]
+    )
     return mask
-
-
-
 
 
 def get_mask_account(account_number: str) -> str:
@@ -12,7 +20,8 @@ def get_mask_account(account_number: str) -> str:
     mask_account = "Счет" + " " + "**" + account_number[-4:]
     return mask_account
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     card_number = str("Visa Classic 6831982476737658")
     print(get_mask_card_number(card_number))
     account_number = "Счет 73654108430135874305"
